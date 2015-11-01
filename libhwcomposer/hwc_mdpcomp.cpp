6d58d8e9f0f87d25278de07ed7c18e568db44cec
Steve Kondik <steve@cyngn.com>
2015-11-01 02:51:41 -0800

    display: [FIXUP] Fix issues when building with Clang
     * Fix up all warnings to enable build with Werror too
    
    Change-Id: Ibd1bcb3ba15677c7ea5570ae583235e0d3962b54
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -1105,7 +1105,8 @@ bool MDPComp::loadBasedComp(hwc_context_t *ctx,
 
 bool MDPComp::isLoadBasedCompDoable(hwc_context_t *ctx,
         hwc_display_contents_1_t* /*list*/) {
-    if(mDpy or isSecurePresent(ctx, mDpy)) {
+    if(mDpy or isSecurePresent(ctx, mDpy) or
+            isYuvPresent(ctx, mDpy)) {
         return false;
     }
     return true;
